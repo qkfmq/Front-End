e02390ca1ea58b025e896fabb98ab71ab61680d2
qkfmq <ko10975796@gmail.com>
2024-06-24 20:52:03 +0900

    2번째

---
+++ js/lecture06.js
@@ -0,0 +1,23 @@
+//07번
+/*var man = { name:"홍길동",age : 20, height:180};
+man.name="이몽룡"; // 이름에 값을 홍길동에서 이몽룡으로 바꿀때
+man["age"]= 15;  // age 값을 20에서 15로 변경할때*/
+
+//08번
+/*var uninitialized_var;
+
+var obj={
+    x:1,
+    y:2
+};*/
+
+/*var a=1;
+console.log(++a); a++는 a가 실행되고 그다음 실행으로 a가 2이다.
+console.log(a); a= a+1*/
+
+10-2
+2*3 > 4+5 && 6/3 == 2 || !false
+
+height >= 180 && gender == "male" || height >= 165 && gender == "female"
+
+(height >= 180 && gender == "male") || (height >= 165 && gender == "female")
